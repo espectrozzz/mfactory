51bc95cf41a58d4d863c42b6f3cb2015daff582e
Manuel Enrique Alfaro Madrid <alfaro_9619@hotmail.com>
2025-04-28 13:38:52 -0600

    feat(FardosInventoryTable.vue): Add filter to fardo table

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -85,7 +85,7 @@
           </li>
         </ul>
       </div>
-      <div class="absolute text-white bottom-0 p-2">v1.4.0</div>
+      <div class="absolute text-white bottom-0 p-2">v1.5.1</div>
     </div>
   </div>
 </template>
